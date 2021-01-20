0529682aacc53794144e4b3bca80301f1eede4fa
Oordell <olliverordell@hotmail.com>
2021-01-20 11:28:57 +0100

    Added colors module

---
+++ app/config/colors.tsx
@@ -0,0 +1,10 @@
+export default Object.freeze({
+  background: '#fff',
+  black: '#000',
+  border: '#4f4f4f',
+  card: '#f8f4f4',
+  notification: '#F44444',
+  primary: '#6C98FF',
+  text: '#0c0c0c',
+  white: '#fff',
+});
