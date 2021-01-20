bd9ce98bef1541751f5dc9da9e3afd85a880f5de
Oordell <olliverordell@hotmail.com>
2021-01-20 16:43:39 +0100

    Installed react-navigation

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import colors from './app/config/colors';
 import RegisterScreen from './app/screens/RegisterScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -17,10 +18,10 @@ Icon.loadFont();
 
 const App = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
       <RegisterScreen />
-    </>
+    </NavigationContainer>
   );
 };
 
