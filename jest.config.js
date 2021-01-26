ef86557fffe990905b25e4392e0ea59dca289195
Oordell <olliverordell@hotmail.com>
2021-01-26 15:28:17 +0100

    Trying to get Jest up and running, but still have some issues

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest/setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/jest/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/jest//assetsTransformer.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native-firebase/app|@react-native-firebase/auth)',
+  ],
 };
