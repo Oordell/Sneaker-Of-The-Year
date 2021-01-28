6df07f3bb8d0a936992fc365b3ae61b5f8fce483
Oordell <olliverordell@hotmail.com>
2021-01-28 13:38:40 +0100

    Added four small buttons for adding the sneaker to different collections in the app.

--- app/screens/SneakerDetailsScreen.tsx
+++ app/screens/SneakerDetailsScreen.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, ScrollView} from 'react-native';
 import AppText from '../components/AppText';
-import AppButton from '../components/buttons/AppButton';
+import AppButtonSmall from '../components/buttons/AppButtonSmall';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
 import defaultStyles from '../config/style';
@@ -13,42 +13,76 @@ interface Props {
 const SneakerDetailsScreen: FC<Props> = ({route}) => {
   const sneaker = route.params;
 
+  const handleAddToTopTenPressed = () => {
+    console.log('Add to top 10');
+  };
+
+  const handleAddToClosetPressed = () => {
+    console.log('Add to Closet');
+  };
+
+  const handleAddToWishListPressed = () => {
+    console.log('Add to Wish List');
+  };
+
+  const handleAddToFavoritesPressed = () => {
+    console.log('Add to Favorites');
+  };
+
   return (
     <Screen style={styles.container}>
-      <View style={[styles.imageAndTitle, defaultStyles.shadows]}>
-        <Image
-          style={styles.image}
-          source={{uri: sneaker.media.smallImageUrl}}
-        />
-        <AppText style={styles.title}>{sneaker.title}</AppText>
-      </View>
-      <View style={[styles.details, defaultStyles.shadows]}>
-        <AppText>
-          <AppText style={styles.boldText}>Brand:</AppText> {sneaker.brand}
-        </AppText>
-        <AppText>
-          <AppText style={styles.boldText}>Release Date:</AppText>{' '}
-          {sneaker.releaseDate}
-        </AppText>
-        <AppText>
-          <AppText style={styles.boldText}>Retail Price:</AppText>{' '}
-          {sneaker.retailPrice}
-        </AppText>
-        <AppText>
-          <AppText style={styles.boldText}>Style ID:</AppText> {sneaker.styleId}
-        </AppText>
-        <AppText>
-          <AppText style={styles.boldText}>Colorway:</AppText>{' '}
-          {sneaker.colorway}
-        </AppText>
-      </View>
-      <View style={styles.buttonContainer}>
-        <AppButton
-          title="Add to top 10"
-          iconName="plus"
-          onPress={() => console.log('Added pressed')}
-        />
-      </View>
+      <ScrollView>
+        <View style={[styles.imageAndTitle, defaultStyles.shadows]}>
+          <Image
+            style={styles.image}
+            source={{uri: sneaker.media.smallImageUrl}}
+          />
+          <AppText style={styles.title}>{sneaker.title}</AppText>
+          <View style={styles.buttonContainer}>
+            <AppButtonSmall
+              style={styles.buttonsSmall}
+              iconName="trophy"
+              onPress={handleAddToTopTenPressed}
+            />
+            <AppButtonSmall
+              style={styles.buttonsSmall}
+              iconName="wardrobe"
+              onPress={handleAddToClosetPressed}
+            />
+            <AppButtonSmall
+              style={styles.buttonsSmall}
+              iconName="playlist-plus"
+              onPress={handleAddToWishListPressed}
+            />
+            <AppButtonSmall
+              style={styles.buttonsSmall}
+              iconName="heart"
+              onPress={handleAddToFavoritesPressed}
+            />
+          </View>
+        </View>
+        <View style={[styles.details, defaultStyles.shadows]}>
+          <AppText>
+            <AppText style={styles.boldText}>Brand:</AppText> {sneaker.brand}
+          </AppText>
+          <AppText>
+            <AppText style={styles.boldText}>Release Date:</AppText>{' '}
+            {sneaker.releaseDate}
+          </AppText>
+          <AppText>
+            <AppText style={styles.boldText}>Retail Price:</AppText>{' '}
+            {sneaker.retailPrice}
+          </AppText>
+          <AppText>
+            <AppText style={styles.boldText}>Style ID:</AppText>{' '}
+            {sneaker.styleId}
+          </AppText>
+          <AppText>
+            <AppText style={styles.boldText}>Colorway:</AppText>{' '}
+            {sneaker.colorway}
+          </AppText>
+        </View>
+      </ScrollView>
     </Screen>
   );
 };
@@ -58,8 +92,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   buttonContainer: {
-    margin: 20,
-    bottom: 20,
+    padding: 20,
+    paddingBottom: 10,
+    flexDirection: 'row',
+  },
+  buttonsSmall: {
+    marginHorizontal: 10,
   },
   container: {
     flex: 1,
