89905051182aa271baf72af9f9a4f8d3da0a5372
Oordell <olliverordell@hotmail.com>
2021-01-28 16:04:48 +0100

    Made the navigation tab bar better

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -2,6 +2,8 @@ import React, {FC} from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import HomeNavigator from './HomeNavigator';
 import TopTenScreen from '../screens/TopTenScreen';
+import {Icon} from 'react-native-elements';
+import colors from '../config/colors';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -9,9 +11,39 @@ interface Props {}
 
 const AppNavigator: FC<Props> = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeNavigator} />
-      <Tab.Screen name="Top 10" component={TopTenScreen} />
+    <Tab.Navigator
+      shifting={true}
+      activeColor={colors.text}
+      sceneAnimationEnabled={true}
+      barStyle={{backgroundColor: colors.text_veryLight}}>
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon
+              name="home"
+              color={color}
+              size={25}
+              type="material-community"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Top 10"
+        component={TopTenScreen}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon
+              name="trophy"
+              color={color}
+              size={25}
+              type="material-community"
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
